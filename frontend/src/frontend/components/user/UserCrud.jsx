bf7f5f5e1ad3d167dacaf1b6588c336ce8afa948
Ivan Yanez <ivanyanez_sjc@hotmail.com>
2022-02-17 12:39:36 -0300

    Inserindo Formulario

--- frontend/src/frontend/components/user/UserCrud.jsx
+++ frontend/src/frontend/components/user/UserCrud.jsx
@@ -62,32 +62,39 @@ export default class UserCrud extends Component{
                     <div className="col-12 col-md-6">
                         <div className="form-group">
                             <label>Nome</label>
-                            <input type="text" className="form-control" name="name"
-                            value={this.state.user.name}
-                            onChange={e=>this.UpdateField(e)}
-                            placeholder="Digite o Nome"/>
+                            <input type="text" className="form-control"
+                                name="name"
+                                value={this.state.user.name}
+                                onChange={e => this.updateField(e)}
+                                placeholder="Digite o nome..." />
                         </div>
-
                     </div>
-                    
+
                     <div className="col-12 col-md-6">
                         <div className="form-group">
                             <label>E-mail</label>
-                            <input type="text" className="form-control" name="email"
-                            value={this.state.user.email}
-                            onChange={e=>this.UpdateField(e)}
-                            placeholder="Digite o E-mail"/>
-
+                            <input type="text" className="form-control"
+                                name="email"
+                                value={this.state.user.email}
+                                onChange={e => this.updateField(e)}
+                                placeholder="Digite o e-mail..." />
                         </div>
                     </div>
+                </div>
 
-                    <hr/>
+                <hr />
+                <div className="row">
+                    <div className="col-12 d-flex justify-content-end">
+                        <button className="btn btn-primary"
+                            onClick={e => this.save(e)}>
+                            Salvar
+                        </button>
 
-                    <div className="row">
-                        
+                        <button className="btn btn-secondary ml-2"
+                            onClick={e => this.clear(e)}>
+                            Cancelar
+                        </button>
                     </div>
-                    
-                    
                 </div>
             </div>
         )
@@ -99,8 +106,8 @@ export default class UserCrud extends Component{
 
         return(
 
-           <Main{...headerProps}> 
-                Cadastro de Usuario
+           <Main {...headerProps}> 
+               {this.renderForm()}
            
            </Main>
 
